f980811aa4c0676bd7d4ddd6f331ddf1fc6bf21f
Andrew Bunday <andrew.bunday@gmail.com>
2022-01-03 16:43:46 +0000

    initial commit to get into github

---
+++ config.el
@@ -0,0 +1,11 @@
+;; git-auto-commit-layer/config.el
+;;
+;;
+
+;; make the layer magically pick up new files.
+(setq gac-automatically-add-new-files-p t)
+
+;; back up to the remote on save.
+(setq gac-automatically-push-p t)
+
+
